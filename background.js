d74ecbd0da18841b4c88473d900b18b251126dfd
raouf <raouf.belazzougui@gmail.com>
2025-06-25 23:45:29 +0100

    from git

---
+++ background.js
@@ -0,0 +1,6 @@
+// if (chrome && chrome.action) {
+//   chrome.action.setPopup({
+//     popup: "popup.html"
+//   });
+// }
+// this was supposed to make things round!!
---
+++ popup.js
@@ -0,0 +1,1 @@
+//i hope i could make this lmao
