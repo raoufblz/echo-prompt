66b2682c9997c2fc53edc74ebab0a3ff8a5cc13c
raouf <raouf.belazzougui@gmail.com>
2025-06-26 23:05:35 +0100

    i m getting the hang of it

--- popup.js
+++ popup.js
@@ -1,11 +1,29 @@
 //i hope i could make this lmao
 
-const prompt_container = document.querySelector(".container");
-const createbtn = document.querySelector(".tab");
 
-createbtn.addEventListener("click", ()=> {
-    let inputbox = document.createElement("p");
-    inputbox.className = "input-box";
-    inputbox.setAttribute("contenteditable" , "true");
-    prompt_container.appendChild(inputbox)
+// need to repair this
+
+// const prompt_container = document.querySelector(".container");
+// const createbtn = document.querySelector(".tab");
+
+// createbtn.addEventListener("click", ()=> {
+//     let inputbox = document.createElement("p");
+//     inputbox.className = "input-box";
+//     inputbox.setAttribute("contenteditable" , "true");
+//     prompt_container.appendChild(inputbox)
+// })
+
+
+const tabs = document.querySelectorAll('.tab'); 
+const all_content = document.querySelectorAll('.tab-content');
+
+tabs.forEach((tab,index)=>{
+tab.addEventListener('click', (e)=>{
+        tabs.forEach(tab=>{tab.classList.remove('active')});
+        tab.classList.add('active');
+   
+all_content.forEach(content =>{content.classList.remove('active')});
+all_content[index].classList.add('active');
+})
+
 })
