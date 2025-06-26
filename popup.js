421fe2d5f60aea4adab40349825a3e05c4bb66e6
raouf <raouf.belazzougui@gmail.com>
2025-06-26 14:10:32 +0100

    from git

--- popup.js
+++ popup.js
@@ -1,1 +1,11 @@
 //i hope i could make this lmao
+
+const prompt_container = document.querySelector(".container");
+const createbtn = document.querySelector(".tab");
+
+createbtn.addEventListener("click", ()=> {
+    let inputbox = document.createElement("p");
+    inputbox.className = "input-box";
+    inputbox.setAttribute("contenteditable" , "true");
+    prompt_container.appendChild(inputbox)
+})
